143c837d4e8bd0fff4e358d446df7335138daba8
byronogis <byronogis@outlook.com>
2024-04-02 23:34:43 +0800

    chore!: update eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -8,5 +8,20 @@ export default withNuxt(
       unocss: true, // dep: @unocss/eslint-config
       formatters: true, // dep: eslint-plugin-format
     },
+    {
+      rules: {
+        'no-console': 'warn',
+
+        /**
+         * Always use curly braces for blocks and new line.
+         * @see https://eslint.org/docs/latest/rules/curly#all
+         * @see https://eslint.style/rules/default/max-statements-per-line#max
+         * @see https://eslint.style/rules/default/brace-style#stroustrup
+         */
+        'curly': ['error', 'all'],
+        '@stylistic/max-statements-per-line': ['error', { max: 1 }],
+        '@stylistic/brace-style': ['error', 'stroustrup'],
+      },
+    },
   ),
 )
