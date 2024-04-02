57e02f65a0c1210afabdf5c64fa361813e12a089
byronogis <byronogis@outlook.com>
2024-04-02 23:30:20 +0800

    feat: page/layout transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+  },
+  /**
+   * Nuxt will include it to all the pages of your application.
+   * @see https://nuxt.com/docs/getting-started/styling#the-css-property
+   */
+  css: [
+    '~/assets/styles/transition.css',
+  ],
+  debug: false,
   devtools: { enabled: true },
   ssr: false,
 })
---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <span>about page</span>
+    <button @click="counterPlus()">counter + </button>
+  </div>
+</template>
