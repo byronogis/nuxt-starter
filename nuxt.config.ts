83aac20f91cc7bc8964df81ccbec0ed81ff097da
byronogis <byronogis@outlook.com>
2024-04-02 23:31:00 +0800

    chore: update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    baseURL: '/',
+    head: {
+      meta: [
+        { "charset": "utf-8" },
+        { name: 'viewport', content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0' },
+        { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge,chrome=1' },
+        { name: 'renderer', content: 'webkit' },
+      ],
+      link: [
+        // { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+      ],
+    },
     pageTransition: { name: 'page', mode: 'out-in' },
     layoutTransition: { name: 'layout', mode: 'out-in' },
+  },
+  /**
+   * It will be merged with app.config file as default value.
+   * @see https://nuxt.com/docs/getting-started/configuration#app-configuration
+   */
+  appConfig: {
+    foo: 'bar',
   },
   /**
    * Nuxt will include it to all the pages of your application.
@@ -12,6 +31,19 @@ export default defineNuxtConfig({
     '~/assets/styles/transition.css',
   ],
   debug: false,
+  devServer: {
+    host: [ 'localhost' ],
+    port: 3000,
+  },
   devtools: { enabled: true },
+  /**
+   * By default, Nuxt comes with the following plugins already pre-configured:
+   * postcss-import: Improves the @import rule
+   * postcss-url: Transforms url() statements
+   * autoprefixer: Automatically adds vendor prefixes
+   * cssnano: Minification and purge
+   * @see https://nuxt.com/docs/getting-started/styling#using-postcss
+   */
+  postcss: {},
   ssr: false,
 })
